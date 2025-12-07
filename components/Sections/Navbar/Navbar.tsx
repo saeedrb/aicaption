a915645908ca78999582bd20679339461b61eb7d
saeedrb <saeed.rb71@gmail.com>
2025-12-07 10:07:27 +0330

    add navbar component

---
+++ components/Sections/Navbar/Navbar.tsx
@@ -0,0 +1,20 @@
+import Link from "next/link";
+import styles from "./Navbar.module.css";
+
+const Navbar: React.FC = () => {
+  return (
+    <div className={styles.navbar}>
+      <div className={styles.logo}>
+        <img src={"./logo.png"} alt="Logo" className={styles.logoImage} />
+        <span className={styles.logoText}>خوشنویس</span>
+      </div>
+      <div className={styles.login}>
+        <Link href="/login" className={styles.loginLink}>
+          <span> ورود / ثبت نام</span>
+        </Link>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
